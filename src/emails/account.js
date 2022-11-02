bbcf6f8b6d6b536ae0922551cb22ce7391f56ace
Sergio Andrés Castellanos Landazábal <serancaslan@gmail.com>
2022-11-02 10:43:14 -0400

    init comit

---
+++ src/emails/account.js
@@ -0,0 +1,46 @@
+const API_KEY = process.env.API_KEY
+const DOMAIN = process.env.DOMAIN
+process.env.API_KEY
+
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({username: 'api', key: API_KEY});
+
+const sendWelcomeEmail = (email, name) =>{
+    const messageData = {
+        from: 'Excited User <serancaslan@gmail.com>',
+        to: email,
+        subject: 'Thanks for joining in',
+        text: `Welcome to the app ${name}`
+      };
+    client.messages.create(DOMAIN, messageData)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+}
+
+const sendCancelationEmail = (email, name) =>{
+    const messageData = {
+        from: 'Excited User <serancaslan@gmail.com>',
+        to: email,
+        subject: 'Cancelation account',
+        text: `Hi ${name}, can you tell us the reason for cancel your account. Thanks`
+      };
+    client.messages.create(DOMAIN, messageData)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
